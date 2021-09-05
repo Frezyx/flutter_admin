9ffa03b073087f30be2ba5d7e1e05fe3c3a488e2
Stanislav Ilin <youngfrezyx@gmail.com>
2021-09-05 16:50:45 +0500

    Migrate from package_info to package_info

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,12 @@
 import FlutterMacOS
 import Foundation
 
-import package_info
+import package_info_plus_macos
 import path_provider_macos
 import shared_preferences_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FLTPackageInfoPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlugin"))
+  FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
--- lib/src/widgets/action_bodies/package_info_body.dart
+++ lib/src/widgets/action_bodies/package_info_body.dart
@@ -2,7 +2,7 @@ import 'package:debug_friend/src/services/package_info/service.dart';
 import 'package:debug_friend/src/widgets/action_bodies/common_action_body.dart';
 import 'package:debug_friend/src/widgets/widgets.dart';
 import 'package:flutter/material.dart';
-import 'package:package_info/package_info.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 class PackageInfoBody extends StatefulWidget {
   const PackageInfoBody({Key? key}) : super(key: key);
