e8c8b9625fa8d7fcace1464bb884917e8cc1cd95
Stanislav Ilin <youngfrezyx@gmail.com>
2022-09-04 13:14:05 +0500

    Implement device data loading

--- packages/debug_friend/lib/src/widgets/actions/app_info/app_info_controller.dart
+++ packages/debug_friend/lib/src/widgets/actions/app_info/app_info_controller.dart
@@ -1,9 +1,13 @@
+import 'dart:io';
+
 import 'package:debug_friend/src/utils/talker.dart';
 import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 
 class AppInfoController extends ChangeNotifier {
+  static final DeviceInfoPlugin _deviceInfo = DeviceInfoPlugin();
+
   AndroidDeviceInfo? _androidDeviceInfo;
   IosDeviceInfo? _iosDeviceInfo;
   PackageInfo? _packageInfo;
@@ -14,6 +18,12 @@ class AppInfoController extends ChangeNotifier {
 
   Future<void> loadAll() async {
     await _loadPackageInfo();
+    if (Platform.isIOS) {
+      _iosDeviceInfo = await _deviceInfo.iosInfo;
+    } else if (Platform.isAndroid) {
+      _androidDeviceInfo = await _deviceInfo.androidInfo;
+    }
+    notifyListeners();
   }
 
   Future<void> _loadPackageInfo() async {
