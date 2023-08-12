d094d98323b418f1dab64eaf91ea312928142f82
Stanislav Ilin <youngfrezyx@gmail.com>
2023-08-13 01:36:55 +0500

    Update file_service.dart

--- packages/debug_friend/lib/src/services/file/src/file_service.dart
+++ packages/debug_friend/lib/src/services/file/src/file_service.dart
@@ -4,27 +4,37 @@ import 'package:debug_friend/src/services/file/service.dart';
 import 'package:path_provider/path_provider.dart';
 
 class FileService implements AbstractFileService {
+  FileService._();
+
   static final AbstractFileService _service = FileService._();
   static AbstractFileService get instance => _service;
 
   @override
-  Future<void> deleteAppDir() async {
+  Future<void> deleteAppDir() => _deleteAppDir();
+
+  Future<void> _deleteAppDir() async {
     final appDir = await getApplicationSupportDirectory();
+
     if (appDir.existsSync()) {
       appDir.deleteSync(recursive: true);
     }
   }
 
   @override
-  Future<void> deleteCacheDir() async {
+  Future<void> deleteCacheDir() => _deleteCacheDir();
+
+  Future<void> _deleteCacheDir() async {
     final cacheDir = await getTemporaryDirectory();
+
     if (cacheDir.existsSync()) {
       cacheDir.deleteSync(recursive: true);
     }
   }
 
   @override
-  Future<List<File>> getFiles() async {
+  Future<List<File>> getFiles() => _getFilesList();
+
+  Future<List<File>> _getFilesList() async {
     final fileModels = <File>[];
     final cacheDir = await getTemporaryDirectory();
     final files = cacheDir.listSync();
